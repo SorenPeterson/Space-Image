7dfd6b7dc38d76925ba98d3e840da291b3bc0357
Soren Peterson <soren468@gmail.com>
2015-02-05 21:50:01 -0800

    create images controller

---
+++ app/controllers/images.rb
@@ -0,0 +1,27 @@
+# Display an image selected for user
+get '/images' do
+end
+
+# Page to upload new image
+get '/images/new' do
+end
+
+# Add image to the database
+post '/images' do
+end
+
+# View a particular photo
+get '/photos/:id' do
+end
+
+# Edit page for a particular photo
+get '/photos/:id/edit' do
+end
+
+# Update a photo
+put '/photos/:id' do
+end
+
+# Delete a photo
+delete '/photos/:id' do
+end
