9143e6c8af78101198c6afea501007e2c1527967
Soren Peterson <soren468@gmail.com>
2015-02-07 01:44:44 -0800

    Convert forms to use actual button inputs. Html

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,26 @@
-$(document).ready(function() {
-  $("div.login").click(function() {
-    $("span.loginerrors").html('')
-    parameters = {
-      user_name: $("form.login input[name='user_name']").val(),
-      password: $("form.login input[name='password']").val()
+function login() {
+  $("span.loginerrors").html('')
+  parameters = {
+    user_name: $("form.login input[name='user_name']").val(),
+    password: $("form.login input[name='password']").val()
+  }
+
+  $.post("/users/login", parameters, function(response) {
+    if(response.successful) {
+      $("header").html(response.content);
+    } else {
+      $("span.loginerrors").html(response.content);
     }
-
-    $.post("/users/login", parameters, function(response) {
-      if(response.successful) {
-        $("header").html(response.content);
-      } else {
-        $("span.loginerrors").html(response.content);
-      }
-    });
   });
+}
 
-  $("div.signup").click(function() {
-    parameters = {
-      user_name: $("form.signup input[name='user_name']").val(),
-      email: $("form.signup input[name='email']").val()
-    }
+function signup() {
+  parameters = {
+    user_name: $("form.signup input[name='user_name']").val(),
+    email: $("form.signup input[name='email']").val()
+  }
 
-    $.post("/users", parameters, function(response) {
-      $("span.signuperrors").html(response.content);
-    });
+  $.post("/users", parameters, function(response) {
+    $("span.signuperrors").html(response.content);
   });
-});
+}
