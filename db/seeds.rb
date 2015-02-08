fd018670cd74fa1ef3374fec6885d13b84da8104
Soren Peterson <soren468@gmail.com>
2015-02-08 15:47:03 -0800

    Seed pull all images from cloudinary automatically

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-require 'ap'
-ap resources = Cloudinary::Api.resources(cloud_name: 'dtxam53uz', api_key: '652441437488924', api_secret: 'qNuJk5DfocBDd52B0emcCK_EDUo')
-resources["resources"].each do |image_info|
-  Image.create(link: image_info["url"])
-end
-ap resources = Cloudinary::Api.resources(cloud_name: 'dtxam53uz', api_key: '652441437488924', api_secret: 'qNuJk5DfocBDd52B0emcCK_EDUo', cursor: resources["next_cursor"])
-resources["resources"].each do |image_info|
-  Image.create(link: image_info["url"])
+cursor = nil
+loop do
+  resources = Cloudinary::Api.resources(cloud_name: 'dtxam53uz', api_key: '652441437488924', api_secret: 'qNuJk5DfocBDd52B0emcCK_EDUo', next_cursor: cursor)
+  resources["resources"].each do |image_info|
+    Image.create(link: image_info["url"])
+  end
+  cursor = resources["next_cursor"]
+  break if cursor == nil
 end
